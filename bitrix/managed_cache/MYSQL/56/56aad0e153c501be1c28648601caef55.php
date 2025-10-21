994ac485302a80a0903e321bb29bfe46efaac54d
bakkyyy <55308123+bakkyyy@users.noreply.github.com>
2025-10-21 21:58:22 +0500

    Delete bitrix directory

--- bitrix/managed_cache/MYSQL/56/56aad0e153c501be1c28648601caef55.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001760957401';
-$dateexpire = '001763549401';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:13:"ADMIN_SECTION";b:1;s:14:"PUBLIC_SECTION";b:1;}}';
-return true;
-?>
--- bitrix/managed_cache/MYSQL/orm_b_group/f8/f8805d87b60c31893633ddb9d66a37a5.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001760963851';
-$dateexpire = '001761050251';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{i:0;a:1:{s:2:"ID";s:1:"2";}}}';
-return true;
-?>
